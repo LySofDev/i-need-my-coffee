ee169d5a65e28ef4552977d605b082e5926d6d0d
Esteban Hernandez <hernandez.dev.services@gmail.com>
2018-06-17 15:55:44 -0400

    Separated authentication into new file

--- server/src/auth.js
+++ server/src/auth.js
@@ -1,29 +1,60 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').load();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
 
-const {
-  CLIENT_ID="FOO",
-  CLIENT_SECRET="BAR"
-} = process.env;
+module.exports = function(app) {
+  // Configure cookies for session
+  app.use(cookieParser());
+  app.use(cookieSession({
+    name: 'incf-session',
+    keys: [ process.env.COOKIE_SECRET ]
+  }));
 
-module.exports = function(passport) {
+  // Configure Passport and Google
+  passport.use(new GoogleStrategy({
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: process.env.CALLBACK_URL
+    },
+    function(accessToken, refreshToken, profile, done) {
+      return done(null, {
+        name: profile.displayName,
+        token: accessToken
+      });
+    }
+  ));
 
-  passport.serializeUser((user, done) => {
+  // User serialization
+  passport.serializeUser(function(user, done) {
     done(null, user);
   });
 
-  passport.deserializeUser((user, done) => {
+  passport.deserializeUser(function(user, done) {
     done(null, user);
   });
 
-  passport.use(
-    new GoogleStrategy(
-      {
-        clientID: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-        callbackURL: process.env.CALLBACK_URL
-      },
-      (token, refreshToken, profile, done) => done(null, { profile, token })
-    )
+  // Initialize the authentication strategy
+  app.use(passport.initialize());
+
+  // Google One-Click authentication
+  app.get('/auth/google', passport.authenticate('google', {
+    scope: ['https://www.googleapis.com/auth/plus.login']
+  }));
+
+  // Authentication Callback
+  app.get('/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/errors/401' }),
+    function(req, res) {
+      req.session.token = req.user.token
+      res.redirect('/');
+    }
   );
 
+  // Logout callback
+  app.get('/auth/logout', (req, res) => {
+    res.cookie('token', '');
+    res.json({ data: 'Session cleared.' });
+  });
 }
--- server/src/server.js
+++ server/src/server.js
@@ -1,72 +1,17 @@
-if (process.env.NODE_ENV !== 'production') require('dotenv').load();
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-const cookieParser = require('cookie-parser');
-const cookieSession = require('cookie-session');
 const { err404, err401, err403 } = require('./errors');
-
+const auth = require('./auth');
 const app = express();
-app.use(cookieParser());
-app.use(cookieSession({
-  name: 'incf-session',
-  keys: [ process.env.COOKIE_SECRET ]
-}));
 
-passport.use(new GoogleStrategy({
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: process.env.CALLBACK_URL
-  },
-  function(accessToken, refreshToken, profile, done) {
-    return done(null, {
-      name: profile.displayName,
-      token: accessToken
-    });
-  }
-));
+// Setup Google One-Click authentication
+auth(app);
 
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(user, done) {
-  done(null, user);
-});
-
-app.use(passport.initialize());
-
-app.get('/auth/google', passport.authenticate('google', {
-  scope: ['https://www.googleapis.com/auth/plus.login']
-}));
-
-app.get('/auth/google/callback',
-  passport.authenticate('google', { failureRedirect: '/' }),
-  function(req, res) {
-    req.session.token = req.user.token
-    res.redirect('/');
-  }
-);
-
-app.get('/auth/logout', (req, res) => {
-  res.cookie('token', '');
-  res.json({ data: 'Session cleared.' });
-});
-
-app.get('/', (req, res) => {
-  if (req.session.token) {
-    res.cookie('token', req.session.token);
-    res.json({ data: ['Cookie set.'] });
-  } else {
-    res.cookie('token', '');
-    res.json({ data: 'session cookie not set'});
-  }
-});
-
+// Validate that the server is running.
 app.get('/status', (req, res) => {
   res.status(200).json({});
 });
 
+// Common error messages
 app.get('/errors/401', err401);
 app.get('/errors/403', err403);
 app.use(err404);
